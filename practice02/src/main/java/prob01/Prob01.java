b006e5b1d5450bf0ba99072077a6db2012100fa0
HEE-JE <windba78@naver.com>
2022-04-26 16:49:28 +0900

    practice02

---
+++ practice02/src/main/java/prob01/Prob01.java
@@ -0,0 +1,20 @@
+package prob01;
+
+import java.util.Scanner;
+
+public class Prob01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		System.out.println("금액:");
+		int num = scanner.nextInt();
+		for (int i = 0; i < MONEYS.length; i++) {
+			System.out.print(MONEYS[i] + "원 : ");
+			int count = num / MONEYS[i];
+			num -= count * MONEYS[i];
+			System.out.println(count + "개");
+		}
+		scanner.close();
+	}
+}
---
+++ practice02/src/main/java/prob04/Prob04.java
@@ -0,0 +1,27 @@
+package prob04;
+
+public class Prob04 {
+
+	public static void main(String[] args) {
+		char[] c1 = reverse("Hello World");
+		printCharArray(c1);
+
+		char[] c2 = reverse("Java Programming!");
+		printCharArray(c2);
+	}
+
+	public static char[] reverse(String str) {
+		/* 코드를 완성합니다 */
+		char a[] = str.toCharArray();
+		for (int i = 0; i < str.length(); i++) {
+			int reverse = str.length() - i;
+			a[i] = str.toCharArray()[reverse - 1];
+		}
+		return a;
+	}
+	
+	public static void printCharArray(char[] array) {
+		/* 코드를 완성합니다 */
+		System.out.println(array);
+	}
+}

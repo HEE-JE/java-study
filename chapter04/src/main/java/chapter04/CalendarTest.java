46c858542fe18bf77eed0793786e0e30586ac957
HEE-JE <windba78@naver.com>
2022-05-02 12:02:16 +0900

    Calendar #finish

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -7,5 +7,37 @@ public class CalendarTest {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		
+		cal.set(Calendar.YEAR, 2022);
+		cal.set(Calendar.MONTH, 11); // 12월(Month + 1)
+		cal.set(Calendar.DATE, 25);
+		printDate(cal);
+		
+		cal.set(2022, 4, 2); // 2022-05-02
+		cal.add(Calendar.DATE, 100);
+		printDate(cal);
+	}
+
+	private static void printDate(Calendar cal) {
+		final String[] DAYS = {"일", "월", "화", "수", "목", "금", "토"};
+		
+		int year = cal.get(Calendar.YEAR);
+		// 월(0~11, +1)
+		int month = cal.get(Calendar.MONTH);
+		int date = cal.get(Calendar.DATE);
+		// 1(일)~7(토)
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+		System.out.println(
+				year + "년 " +
+				(month < 9 ? "0" : "") + (month + 1) + "월 " +
+				(date < 10 ? "0" : "") +  date + "일 " +
+				DAYS[day-1] + "요일 " +
+				hour  + "시 " +
+				minute + "분 " +
+				second + "초"
+				);
 	}
 }

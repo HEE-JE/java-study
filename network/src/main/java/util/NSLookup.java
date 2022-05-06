ecd4948367c7b735b3cc117d6d7b5f974865d44b
HEE-JE <windba78@naver.com>
2022-05-06 17:49:07 +0900

    NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,8 +1,32 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class NSLookup {
 
 	public static void main(String[] args) {
+		Scanner scanner = null;
+		InetAddress[] IP = null;
+		try {
+			scanner = new Scanner(System.in);
+			while (true) {
+				System.out.print("> ");
+				String domain = scanner.nextLine();
+				if ("exit".equals(domain)) {
+					break;
+				}
 
+				IP = InetAddress.getAllByName(domain);
+				for (InetAddress ip : IP) {
+					System.out.println(ip.getHostName() + " : " + ip.getHostAddress());
+				}
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} finally {
+			scanner.close();
+		}
 	}
 }
